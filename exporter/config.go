46c27f2cd3898691b0619bcfdd83890601f4c51c
Mihail Goltsov <migoltsov@ozon.ru>
2021-04-04 18:47:04 +0300

    improve config

--- exporter/config.go
+++ exporter/config.go
@@ -3,8 +3,6 @@ package exporter
 import (
 	"log"
 	"net"
-	"os"
-	"strconv"
 
 	"github.com/spf13/viper"
 )
@@ -17,53 +15,36 @@ type Config struct {
 }
 
 func NewConfig(testConfig bool) Config {
-	const prefix = "BGPEX_"
 	c := Config{}
 
-	viper.SetDefault("ASN", "64512")
+	viper.SetDefault("asn", "64512")
+	viper.SetDefault("RouterID", "1.1.1.1")
+	viper.SetDefault("ListenAddr", "0.0.0.0")
+	viper.SetDefault("PrometheusPort", "9179")
 
-	//ASN
-	if i := os.Getenv(prefix + "ASN"); len(i) > 0 {
-		d, err := strconv.Atoi(i)
-		if err != nil {
-			log.Panic(err, "ASN is invalid")
-		}
-		c.Asn = d
-	} else {
-		c.Asn = 64512
+	viper.SetConfigName("bgp-exporter")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath("/etc/")
+	viper.AddConfigPath(".")
+	viper.ReadInConfig()
+
+	viper.AutomaticEnv()
+	viper.SetEnvPrefix("bgpexp")
+
+	c.Asn = viper.GetInt("asn")
+
+	c.Rid = net.ParseIP(viper.GetString("RouterID"))
+	if c.Rid.To4() == nil {
+		log.Panic("Router ID is invalid")
 	}
 
-	//BGP Router ID
-	if rid := os.Getenv(prefix + "RID"); len(rid) > 0 {
-		c.Rid = net.ParseIP(rid)
-		if c.Rid.To4() == nil {
-			log.Panic("Router ID is invalid")
-		}
+	c.Prom_port = viper.GetInt("PrometheusPort")
+
+	if _, err := net.ResolveTCPAddr("tcp", viper.GetString("ListenAddr")+":179"); err != nil {
+		log.Panic(err, "Listen addres is invalid")
 	} else {
-		c.Rid = net.ParseIP("1.1.1.1")
+		c.Addr = viper.GetString("ListenAddr")
 	}
 
-	//LISTEN ADDR
-	if addr := os.Getenv(prefix + "ADDR"); len(addr) > 0 {
-		if _, err := net.ResolveTCPAddr("tcp", addr); err != nil {
-			log.Panic(err, "ADDR is invalid")
-		}
-		c.Addr = addr
-	} else {
-		c.Addr = "0.0.0.0"
-	}
-
-	//PROMETHEUS PORT
-	if i := os.Getenv(prefix + "PROMETHEUS_PORT"); len(i) > 0 {
-		d, err := strconv.Atoi(i)
-		if err != nil {
-			log.Panic(err, "Port is invalid")
-		}
-		c.Prom_port = d
-	} else {
-		c.Prom_port = 9179
-	}
-
-	c.Asn = viper.GetInt("ASN")
 	return c
 }
